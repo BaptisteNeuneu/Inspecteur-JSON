e0511a73b421cec75b6db2b7d40f0d5348e1a06f
horville <ewen.horville@etu.u-pec.fr>
2023-01-01 21:08:00 +0100

    parser MaJ

--- src/projet/json/JSONParser.java
+++ src/projet/json/JSONParser.java
@@ -74,17 +74,96 @@ public class JSONParser {
         return true;
     }
 
-    private static void treatObject(String object, Tree<Token> tree) {
-
+    private static void treatObject(Tree<Token> tree, String currentPath, String object) {
+        
     }
 
-    private static void treatBoolean(String bool, Tree<Token> tree) {
+    private static void treatTable(Tree<Token> tree, String currentPath, String table) {
+        System.out.println("table");
+    }
 
+    private static void treatBoolean(Tree<Token> tree, String currentPath, String bool) {
+        switch (bool) {
+            case "true":
+                tree.add(new JSONToken(true), currentPath);
+                break;
+            case "false":
+                tree.add(new JSONToken(false), currentPath);
+                break;
+        }
+
+        System.out.println("Boolean : " + bool);
+    }
+
+    private static void treatNull(Tree<Token> tree, String currentPath) {
+        tree.add(new JSONToken(), currentPath);
+        System.out.println("Null");
+    }
+
+    private static void treatString(Tree<Token> tree, String currentPath, String string) {
+        tree.add(new JSONToken(string), currentPath);
+        System.out.println("String : " + string);
+    }
+
+    private static void treatNumber(Tree<Token> tree, String currentPath, String number) {
+        
+        System.out.println("Number : " + number);
+    }
+
+    /**
+     * Retourne la première valeur à traiter et son type dans un tableau
+     * @param json le morceau de donnée JSON à traiter
+     * @return un tableau de String contenant le type et la première valeur
+     * @throws JSONFormatException le morceau de donnée JSON est invalide
+     */
+    private static String[] getFirstValueAndType(String json) throws JSONFormatException {
+        
+        // 10/10 expression régulière
+        Pattern p = Pattern.compile("^\\{(.*)\\}|^\\[(.*)\\]|^(true|false)|^(null)|^\\\"(.*)\\\"|^([0-9\\.\\-\\+eE]*)");
+        Matcher m = p.matcher(json);
+
+        //Cherche la première valeur JSON à traiter, en détermine le type et renvoie l'ensemble.
+        if (m.find()) {
+            /* 1: Objet
+               2: Tableau
+               3: Booléen
+               4: Null
+               5: String
+               6: Nombre
+            */
+
+            int found = 0;
+            for (int i = 1; i < 7; i++) {
+                if (m.group(i) != null) {
+                    found = i;
+                    break;
+                }
+            }
+
+            switch (found) {
+                case 1:
+                    return new String[] {"OBJECT", m.group(found)};
+                case 2:
+                    return new String[] {"TABLE", m.group(found)};
+                case 3:
+                    return new String[] {"BOOLEAN", m.group(found)};
+                case 4:
+                    return new String[] {"NULL"};
+                case 5:
+                    return new String[] {"STRING", m.group(found)};
+                case 6:
+                    return new String[] {"NUMBER", m.group(found)};
+                default:
+                    throw new JSONFormatException("Invalid value formatting");
+            }
+        } else {
+            throw new JSONFormatException("Value type not recognised");
+        }
     }
 
     /***
      * Convertit un fichier JSON sous forme de texte en arborescence.
-     * @param json Le fichier JSON à convertir
+     * @param json le fichier JSON à convertir
      * @return un Tree contenant toute les données
      * @throws JSONFormatException le fichier JSON est malformé
      */
@@ -95,16 +174,32 @@ public class JSONParser {
 
         Tree<Token> tree = new JSONTree();
 
-        Pattern p = Pattern.compile("^\\{(.*)\\}|^(true|false)|^\\[(.*)\\]|^(null)|^\\\"(.*)\\\"");
-        Matcher m = p.matcher(json);
+        String[] valueAndType = getFirstValueAndType(json);
 
-        if (m.find()) {
-            for (int i = 0; i < 6; i++) {
-                if (m.group(i) != null) System.out.print("!");
-                System.out.println(i + ": " + m.group(i));
-            }
-        } else {
-            System.out.println("Number");
+        switch (valueAndType[0]) {
+            case "OBJECT":
+                treatObject(tree, ".", valueAndType[1]);
+                break;
+
+            case "TABLE":
+                treatTable(tree, ".", valueAndType[1]);
+                break;
+
+            case "BOOLEAN":
+                treatBoolean(tree, ".", valueAndType[1]);
+                break;
+
+            case "NULL":
+                treatNull(tree, ".");
+                break;
+
+            case "String":
+                treatString(tree, ".", valueAndType[1]);
+                break;
+
+            case "NUMBER":
+                treatNumber(tree, ".", valueAndType[1]);
+                break;
         }
         
         return tree;
